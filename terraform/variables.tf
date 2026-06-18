09d10588212e6642de70b78cad55e6f7a3c0c3c0
Praj <prajwalbagalkot83@gmail.com>
2026-06-18 21:21:30 +0530

    chore: set ECR repo [skip ci]

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,19 @@
-variable "region" { type = string, default = "ap-south-1" }
-variable "cluster_name" { type = string, default = "devops-demo" }
-variable "instance_types" { type = list(string), default = ["t3.large", "t3.xlarge"] }
-variable "desired_size" { type = number, default = 2 }
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "devops-demo"
+}
+
+variable "instance_types" {
+  type    = list(string)
+  default = ["t3.large", "t3.xlarge"]
+}
+
+variable "desired_size" {
+  type    = number
+  default = 2
+}
